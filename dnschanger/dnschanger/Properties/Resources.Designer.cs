3779a97f74714ef34eab817ea6ec5886e6080c4f
Swotch <109398760+Swotch9399@users.noreply.github.com>
2024-11-24 23:08:57 +0300

    Add files via upload

--- dnschanger/dnschanger/Properties/Resources.Designer.cs
+++ dnschanger/dnschanger/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DNSChanger.Properties {
+namespace dnschanger.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace DNSChanger.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DNSChanger.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("dnschanger.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- dnschanger/dnschanger/Properties/Settings.Designer.cs
+++ dnschanger/dnschanger/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DNSChanger.Properties {
+namespace dnschanger.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
