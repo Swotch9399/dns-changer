2ff51dbe3a9d7141b3c8b0e817b07896bac2de1f
Swotch <109398760+Swotch9399@users.noreply.github.com>
2025-01-26 23:33:25 +0300

    Add files via upload

--- web/script.js
+++ web/script.js
@@ -9,3 +9,19 @@ window.onload = function() {
         window.location.href = "notsupport.html";
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const faqItems = document.querySelectorAll('.faq-item');
+
+    faqItems.forEach(item => {
+        const question = item.querySelector('.faq-question');
+        question.addEventListener('click', () => {
+            if (item.classList.contains('open')) {
+                item.classList.remove('open');
+            } else {
+                faqItems.forEach(i => i.classList.remove('open'));
+                item.classList.add('open');
+            }
+        });
+    });
+});
